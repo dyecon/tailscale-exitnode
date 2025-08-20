ed338f5cd6b137f53d344ee64ab4f3ac90bd82ad
Dyecon <25501036+dyecon@users.noreply.github.com>
2025-08-20 20:46:34 +0900

    Update setup.sh
    Add UDP optimizations

--- setup.sh
+++ setup.sh
@@ -54,4 +54,16 @@ else
     sudo tailscale set --hostname=$NODE_NAME
 fi
 
+# Optimize UDP performance
+# See the following link for details:
+# https://tailscale.com/kb/1320/performance-best-practices#linux-optimizations-for-subnet-routers-and-exit-nodes
+echo "Optimizing UDP performance..."
+NETDEV=$(ip -o route get 8.8.8.8 | cut -f 5 -d " ")
+sudo ethtool -K $NETDEV rx-udp-gro-forwarding on rx-gro-list off
+printf '#!/bin/sh\n\nethtool -K %s rx-udp-gro-forwarding on rx-gro-list off \n' "$(ip -o route get 8.8.8.8 | cut -f 5 -d " ")" | sudo tee /etc/networkd-dispatcher/routable.d/50-tailscale
+sudo chmod 755 /etc/networkd-dispatcher/routable.d/50-tailscale
+sudo /etc/networkd-dispatcher/routable.d/50-tailscale
+test $? -eq 0 || echo "An error occurred while optimizing UDP performance."
+
+echo 
 echo "[✓] Setup complete! This node should now be available as an exit node."
